b3923dd2b7a6d1504b9871119c143f2e605309ec
Lucas Churchill <lucas@agtlucas.com>
2014-05-11 21:48:38 -0300

    Adding watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,9 @@ module.exports = function (grunt) {
 
     // Register group tasks
     grunt.registerTask('build', [ 'jshint', 'compass', 'requirejs', 'copyto', 'i18n' ]);
+
+    grunt.registerTask('w', ['watch', 'compass']);
+
     grunt.registerTask('test', [ 'jshint', 'mochacli' ]);
 
 };
