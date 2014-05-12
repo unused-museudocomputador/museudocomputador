cbfd6402dca6b9ec5c1ad606e63a31e6ef02b207
Lucas Churchill <lucas@agtlucas.com>
2014-05-11 21:19:54 -0300

    Updates..

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+
+requirejs.config({
+    paths: {}
+});
+
+
+require([/* Dependencies */], function () {
+
+    var app = {
+        initialize: function () {
+            // Your code here
+        }
+    };
+
+    app.initialize();
+
+});
+
