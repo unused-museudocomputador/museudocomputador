7c94fea251cd8bb5155159fbc56bb55150a520dd
Lucas Churchill <lucas@agtlucas.com>
2014-07-27 22:35:35 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'about/index'
+
+  get 'sobre', to: 'about#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
