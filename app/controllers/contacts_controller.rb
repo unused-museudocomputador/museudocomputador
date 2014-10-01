0b72469cd078f1db72f32994dbdc721615048ad4
Lucas Churchill <lucas@agtlucas.com>
2014-10-01 08:50:02 -0300

    Update contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,16 @@
 class ContactsController < ApplicationController
+
   def index
+    @contact = Contact.new
   end
 
   def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = "Ok!"
+    else
+      format.json { render :json => { :error => @contact.errors.full_messages }, :status => 422 }
+    end
   end
 end
