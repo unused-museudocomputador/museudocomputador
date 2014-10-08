f130c19fd680f07dfa540cc178313ac8ac9cc54f
Lucas Churchill <lucas@agtlucas.com>
2014-10-08 15:11:11 -0300

    Update Blackbriar

--- app/assets/javascripts/_blackbriar.js
+++ app/assets/javascripts/_blackbriar.js
@@ -30,6 +30,17 @@ var Blackbriar = (function (window, document, undefined) {
     });
   };
 
+  /**
+   * Detect if is mobile
+   */
+  Blackbriar.isMobile = function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      return true;
+    } else {
+      return false
+    }
+  };
+
   return Blackbriar.init();
 
 })(window, document);
