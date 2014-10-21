5a62d917a419ce026bd599be39c9b793708b129e
Lucas Churchill <lucas@agtlucas.com>
2014-10-20 22:38:24 -0200

    Update collection

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,32 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Collection.destroy_all
-Collection.create(title: 'CPU', description: 'Lorem ipsum!', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate repellat eos enim et accusantium adipisci, modi optio autem a sapiente id quae eaque error officia veritatis laudantium, officiis ipsa deserunt!', img: '/m1/intel.jpg')
-Collection.create(title: 'CPU2', description: 'Lorem ipsum 2!', body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut excepturi eligendi, aliquid repudiandae possimus rem quasi omnis doloribus expedita eos iusto quis, hic quisquam repellendus velit ducimus corporis quia placeat?', img: '/m1/intel3.jpg')
+
+Collection.create(
+  title: 'Computador TK2000',
+  description: 'Microcomputador produzido pela empresa brasileira Microdigital Eletrônica Ltda.',
+  body: '<p>O TK2000, produzido pela empresa brasileira Microdigital Eletrônica Ltda, foi um microcomputador apresentado ao público durante a Feira de Informática de 1983 e lançado em 1984.</p>
+  <p>Utilizava a CPU 6502 e era parcialmente compatível com o software e hardware do Apple II+. Em 1985, foi lançada uma versão aperfeiçoada (com opção de 128K), o TK2000 II. Em 1987, foi descontinuado.</p>',
+  img: ''
+)
+
+Collection.create(
+  title: 'GeForce FX5200 LF 128MB AGP',
+  description: 'Placa de vídeo AGP',
+  body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut excepturi eligendi, aliquid repudiandae possimus rem quasi omnis doloribus expedita eos iusto quis, hic quisquam repellendus velit ducimus corporis quia placeat?</p>',
+  img: ''
+)
+
+Collection.create(
+  title: 'SNES - Super Nintendo',
+  description: 'Console da Nintendo',
+  body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut excepturi eligendi, aliquid repudiandae possimus rem quasi omnis doloribus expedita eos iusto quis, hic quisquam repellendus velit ducimus corporis quia placeat?</p>',
+  img: ''
+)
+
+Collection.create(
+  title: 'SNES - Super Nintendo',
+  description: 'Console da Nintendo',
+  body: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut excepturi eligendi, aliquid repudiandae possimus rem quasi omnis doloribus expedita eos iusto quis, hic quisquam repellendus velit ducimus corporis quia placeat?</p>',
+  img: ''
+)
